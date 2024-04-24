c45e4bdb8869e6242dbca8f7a0caf4dfaa177491
newfly101 <newfly101@naver.com>
2024-04-24 12:38:37 +0900

    [2-6] javascript 시간과 문자열

---
+++ JSP/TimeClass.js
@@ -0,0 +1,28 @@
+function sayHello() {
+    console.log("안녕하세요");
+}
+
+// 1초마다 sayHello() function을 호출함
+// setInterval(sayHello, 1000);
+
+// 1초 이후에 sayHello()를 한번 호출함
+setTimeout(sayHello, 1000);
+
+
+let thisTime = new Date().getDate();
+let thisTimeM = new Date().getMinutes();
+let thisTimeS = new Date().getSeconds();
+
+function setTime() {
+    const time = new Date();
+    const timeMin = time.getMinutes();
+    const timeSec = time.getSeconds();
+    const timeH1 = document.querySelector(".time");
+    timeH1.innerText = `${timeMin}:${timeSec}`;
+}
+
+// 시간의 앞에 0을 넣는 경우 다음과 같이 할 수 있다.
+// 유용한 함수 padStart();
+console.log('3'.padStart(2,'0'));
+
+setInterval(setTime, 1000);
