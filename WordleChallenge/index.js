b00e176e634c04ba920a796f1fe0b60b8c1c1089
newfly101 <newfly101@naver.com>
2024-04-25 15:34:27 +0900

    [2-7] javascript wordle 게임 만들기 - 하단의 정답 값이 green인 경우, yellow 값으로 변경되지 않도록 변경함

--- WordleChallenge/index.js
+++ WordleChallenge/index.js
@@ -11,43 +11,61 @@
 let index = 0;
 let attempts = 0;
 
+
 const wordleResult = 'MONEY';
 
 function appStart() {
+    // const displayGameover = () => {
+    //     const div = document.createElement("div");
+    //     div.innerText = "게임이 종료되었습니다.";
+    //     div.style = "display:flex; justify-content:center; align-items:center;";
+    //     document.body.appendChild(div);
+    // }
+    //
+    // const gameover = () => {
+    //     window.removeEventListener("keydown",handleKeyDown);
+    // }
+
+    let totalResult = new Array(4);
+
     const handleEnterKey = (event) => {
         // 정답 확인
         console.log(event);
         let result = '';
         for (let i = 0 ; i < 5; i++) {
-            let strings = document.querySelector(`.board-block[data-index='${attempts}${i}']`);
-            result += strings.innerText.toString();
-        }
-        // console.log(result);
-        handleCheckCorrect(result);
-    }
+            let inputBlock = document.querySelector(`.board-block[data-index='${attempts}${i}']`);
+            let inputText = inputBlock.innerText;
+            let wordBlock = document.querySelector(`.word-key[data-key='${inputText}']`);
+            console.log(wordBlock.innerText);
 
-    function handleCheckCorrect(result) {
-        for (let i = 0; i < result.length; i++) {
-            const boardBlock = document.querySelector(`.board-block[data-index='${attempts}${i}']`);
-            const wordBlock = document.querySelector(`.word-key[data-key='${result[i]}']`);
-            
-            for (let j = 0; j < wordleResult.length; j++) {
-                if (result[i] === wordleResult[i]) {
-                    // console.log(result[i]+"는 정확하게 일치합니다.");
-                    boardBlock.style.backgroundColor = 'green';
-                    wordBlock.style.backgroundColor = 'green';
-                }else if (result[i] === wordleResult[j]) {
-                    // console.log(result[i]+"는 정답 안에 있습니다.");
-                    boardBlock.style.backgroundColor = 'yellow';
-                    if (wordBlock.style.backgroundColor !== 'green') {
-                        wordBlock.style.backgroundColor = 'yellow';
-                    }
+            // 오류 나는 부분 : 하단 wordBlock 의 색상이 계속 변경됨 이것을 정답 시 고정시키는 게 필요함
+            console.log("wordBlock => ",wordBlock);
+            if (inputText === wordleResult[i]) {
+                // 정답 완벽 일치 시 color 색상 설정
+                inputBlock.style.background = "#6aaa64";
+                inputBlock.style.border = "4px solid #6aaa64";
+                wordBlock.style.background = "#6aaa64";
+                wordBlock.style.border = "4px solid #6aaa64";
+                totalResult[i] = inputText;
+            } else if (wordleResult.includes(inputText)) {
+                // 정답 안에 단어 있을 시 color 색상 설정
+                if (!totalResult.includes(inputText)) {
+                    inputBlock.style.background = "#c9b458";
+                    inputBlock.style.border = "4px solid #c9b458";
+                    wordBlock.style.background = "#c9b458";
+                    wordBlock.style.border = "4px solid #c9b458";
+                } else {
+                    inputBlock.style.background = "#c9b458";
+                    inputBlock.style.border = "4px solid #c9b458";
                 }
-                if (boardBlock.style.backgroundColor !== 'yellow' && boardBlock.style.backgroundColor !== 'green') {
-                    boardBlock.style.backgroundColor = 'gray';
-                    wordBlock.style.backgroundColor = 'gray';
-                }
+            } else {
+                // 정답 안에 단어 없을 시 색상 설정
+                inputBlock.style.background = "#787c7e";
+                inputBlock.style.border = "4px solid #787c7e";
+                wordBlock.style.background = "#787c7e";
             }
+            wordBlock.style.color = "white";
+            inputBlock.style.color = "white";
         }
     }
 
@@ -60,6 +78,7 @@ function appStart() {
             if (index <= 4) {
                 thisBlock.innerText = key;
                 index++;
+                thisBlock.style.border = '4px solid black';
             }
         } else if (keyCode === 13 && index === 5) {
             // key = Enter
@@ -71,6 +90,7 @@ function appStart() {
             if (index > 0) {
                 const thisBlock = document.querySelector(`.board-block[data-index='${attempts}${index-1}']`);
                 thisBlock.innerText = '';
+                thisBlock.style.border = '4px solid rgb(211,214,218)';
                 index--;
             }
 
