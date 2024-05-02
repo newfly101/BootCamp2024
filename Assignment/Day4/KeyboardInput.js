dde37b7f0041c5b91596e23c9756b3d2d7691618
Jae-Hong-Kim <newfly101@naver.com>
2024-05-02 16:26:19 +0900

    [Day4] 1주차_신입연수원_DAY4_사수 도움 요청 업무_문제②

---
+++ Assignment/Day4/KeyboardInput.js
@@ -0,0 +1,11 @@
+// 1주차_신입연수원_DAY4_사수 도움 요청 업무_문제②
+// 연도와 달, 날짜까지 표시되는 시계 기능이 추가됐어요. JavaScript를 이용해 구현 부탁드릴게요~!
+
+// const keydownF = (event) => {
+//     console.log(event);
+//     let word = document.querySelector(".check-keyboard");
+//     console.log(event.key, event.keyCode);
+//     word.innerText = event.key.toUpperCase()+", "+event.keyCode;
+// }
+//
+// window.addEventListener("keydown", keydownF);
