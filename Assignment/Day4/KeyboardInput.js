ee7bcd2d127e0326edcfe0c75d3b103004f238ed
Jae-Hong-Kim <newfly101@naver.com>
2024-05-02 16:40:02 +0900

    [Day4] 버그 수정

--- Assignment/Day4/KeyboardInput.js
+++ Assignment/Day4/KeyboardInput.js
@@ -1,7 +1,7 @@
 // 1주차_신입연수원_DAY4_사수 도움 요청 업무_문제②
 // 연도와 달, 날짜까지 표시되는 시계 기능이 추가됐어요. JavaScript를 이용해 구현 부탁드릴게요~!
 
-const toDayViewer = (event) => {
+const toDayViewer = () => {
     setInterval(newDateToday,1000);
 }
 
