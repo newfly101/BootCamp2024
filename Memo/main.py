3c0bbf69788e2652c233751bed7db64b620c12b7
Jae-Hong-Kim <newfly101@naver.com>
2024-05-03 13:45:00 +0900

    [Day5] python 프로젝트 만들기 - 간단한 메모 만들기[3] 서버 구현하기

--- Memo/main.py
+++ Memo/main.py
@@ -1,1 +1,8 @@
 # 서버 구현하기
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseMode1
+
+app = FastAPI()
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
