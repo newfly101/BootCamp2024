e0205cfd465233435653a387dfec75989a482ecd
Jae-Hong-Kim <newfly101@naver.com>
2024-05-04 12:44:35 +0900

    [Day5] python 프로젝트 만들기 - 간단한 메모 만들기[5] 버그 수정 - favicon.ico가 프로젝트 경로상에 없기 때문에 제거

--- Memo/main.py
+++ Memo/main.py
@@ -1,9 +1,13 @@
 # 서버 구현하기
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseMode1
+# from pydantic import BaseMode1
 
 app = FastAPI()
 
 # 루트 경로에 우리의 static 파일에 있는 html을 호스팅 해준다.
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+
+# 서버 실행 방법 : terminal에서 명령어 입력
+# python -m uvicorn main:app --reload
