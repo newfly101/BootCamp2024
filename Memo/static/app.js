38caa6d4e645a90e3c467d0601adc582db08124f
Jae-Hong-Kim <newfly101@naver.com>
2024-05-04 13:03:22 +0900

    [Day5] python 프로젝트 만들기 - 간단한 메모 만들기[6] : 서버에 메모 생성 요청 / memo 생성 요청 시 405 error 발생

--- Memo/static/app.js
+++ Memo/static/app.js
@@ -1,12 +1,31 @@
+async function createMemo(memo) {
+    // 서버에 메모를 생성 요청
+    const res = await fetch("/memos", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json", // 기본적으로 보내야 하는 header
+        },
+        body: JSON.stringify({
+            id: new Date(),
+            content: memo,
+        }),
+    }); // GET 요청
+    const jsonRes = await res.json();
+    console.log(jsonRes);
 
+}
 
-function createMemo(event) {
+function handleSubmit(event) {
     // event를 막는 함수
     event.preventDefault();
-    console.log("동작 check");
+    // input의 값을 그대로 가져와서 create 하기
+    const memo = document.querySelector("#memo-input");
+    console.log("inputMemo값 출력 : ",memo.value);
+    createMemo(memo.value);
+    memo.value = '';
 }
 
 // form에 있는 submit event
 // 제출과 동시에 redirect 됨
 const form = document.querySelector('#memo-form');
-form.addEventListener("submit", createMemo);
+form.addEventListener("submit", handleSubmit);
