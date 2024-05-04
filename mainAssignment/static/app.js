a1fecb2cb1f18e166b1a9645a58ec928373a2409
Jae-Hong-Kim <newfly101@naver.com>
2024-05-04 18:41:55 +0900

    [Day5] 1-12 당근 마켓 clone 만들어보기 - crud 추가하기 : python 서버에 post/get 구현 완료 : page 박살나는 문제

---
+++ mainAssignment/static/app.js
@@ -0,0 +1,46 @@
+function displayMemos(memos) {
+    // data = [{id:"123", content:"inputData"}]
+    const ul = document.querySelector("#chat-ul");
+    const li = document.createElement("li");
+    li.innerText = `[id:${memos.id}] ${memos.content}`;
+    ul.appendChild(li);
+}
+
+async function readMsg() {
+    const res = await fetch("/chat");
+    const jsonRes = await res.json();
+
+    const ul = document.querySelector("#memo-ul");
+    ul.innerHTML = "";
+    jsonRes.forEach(displayMemos);
+
+}
+
+async function chat(msg) {
+    const res = await fetch("/chat", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            date: new Date(),
+            content: msg,
+        }),
+    });
+    const jsonRes = await res.json();
+    console.log(jsonRes)
+}
+
+function handleSubmit(event) {
+    // event 동작 막는 함수
+    event.preventDefault();
+
+    // input값 가져와서 서버로 보내는 함수 작성
+    const sendMessage = document.querySelector("#send-message");
+    chat(sendMessage.value);
+    sendMessage.value = "";
+}
+
+const form = document.querySelector("#chat-form");
+form.addEventListener("submit", handleSubmit);
+
