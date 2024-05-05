162f4c019459261dccc928141cb3bf8ef19e1540
Jae-Hong-Kim <newfly101@naver.com>
2024-05-05 16:18:39 +0900

    [Day5] 1-14 당근 마켓 clone 만들어보기 - html상으로 경로 이동을 위해 만들어진 것

--- mainAssignment/main.py
+++ mainAssignment/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+# from fastapi.templating import Jinja2Templates
 
 class Chat(BaseModel):
     date:str
@@ -15,19 +15,18 @@ app = FastAPI()
 # url 의 이동을 만들기 위해서 실행해야 하는 명령어
 # pip install jinja2
 
-# templates 폴더 만들고 이제 이동할 경로 만들어주기 chat.html 옮겨주기
-templates = Jinja2Templates(directory="templates")
 
 # chat 갖고오기
 @app.get("/chat")
 async def read_chats():
     return chats
 
+
 # chat 서버 통신
-@app.post("/chat")
-async def create_chat(chat: Chat):
-    chats.append(chat)
-    return {"message": "Chat created", "chats": chats}
+# @app.post("/chat")
+# async def create_chat(chat: Chat):
+#     chats.append(chat)
+#     return {"message": "Chat created", "chats": chats}
 
 
 # 루트 경로에 우리의 static 파일에 있는 html을 호스팅 해준다.
