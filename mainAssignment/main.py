bfd23041f48763b5373b5d4bb769ebe074145758
Jae-Hong-Kim <newfly101@naver.com>
2024-05-05 15:50:12 +0900

    [Day5] 1-13 당근 마켓 clone 만들어보기 - fastAPI 이용을 위해 template 옮겨주기

--- mainAssignment/main.py
+++ mainAssignment/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 class Chat(BaseModel):
     date:str
@@ -10,6 +11,12 @@ class Chat(BaseModel):
 chats = []
 
 app = FastAPI()
+
+# url 의 이동을 만들기 위해서 실행해야 하는 명령어
+# pip install jinja2
+
+# templates 폴더 만들고 이제 이동할 경로 만들어주기 chat.html 옮겨주기
+templates = Jinja2Templates(directory="templates")
 
 # chat 갖고오기
 @app.get("/chat")
