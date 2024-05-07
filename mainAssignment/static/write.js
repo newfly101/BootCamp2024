2c23126faf844d7edab671f26d86e2828b42c736
Jae-Hong-Kim <newfly101@naver.com>
2024-05-07 14:19:08 +0900

    [Day9] 1-15 당근마켓 clone 만들어보기 - 글쓰기 페이지 구현하기 - 글쓰기 완료 시 페이지 이동

--- mainAssignment/static/write.js
+++ mainAssignment/static/write.js
@@ -2,16 +2,18 @@ const form = document.getElementById("write-form");
 
 
 async function createNewWrite() {
-    const res = await fetch("/items", {
-        method:'POST',
-        body: new FormData(form)
-    });
-    const jsonRes = await res.json();
-    if (jsonRes === '200') {
-        console.log("글쓰기 생성 : ", jsonRes);
-        window.location.pathname = "/";
-    } else {
-        console.error("이미지 업로드에 실패했습니다.")
+    try{
+        const res = await fetch("/items", {
+            method:'POST',
+            body: new FormData(form)
+        });
+        const jsonRes = await res.json();
+        if (jsonRes === '200') {
+            console.log("글쓰기 생성 : ", jsonRes);
+            window.location.pathname = "/";
+        }
+    } catch (error) {
+        console.error("이미지 업로드에 실패했습니다.", error);
     }
 
 }
