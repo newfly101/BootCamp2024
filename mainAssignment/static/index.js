12067b101022d7e736aea8ce3f84cc09428711f7
Jae-Hong-Kim <newfly101@naver.com>
2024-05-07 21:53:39 +0900

    [Day9] 1-15 당근마켓 clone 만들어보기 - 글쓰기 페이지 구현하기 - FRONT-END에서 'GET' 이용해서 DATA값을 forEach()로 출력

--- mainAssignment/static/index.js
+++ mainAssignment/static/index.js
@@ -1,7 +1,15 @@
+const renderData = (data) => {
+
+    data.forEach( (obj) => console.log(obj));
+}
+
+
 const fetchList = async () => {
     const res = await fetch('/items');
     const data = await res.json();
+
     console.log(data);
+    renderData(data);
 };
 
 fetchList();
