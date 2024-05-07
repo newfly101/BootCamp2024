c86505ec9b291c9217b86b604991813eb3eaf949
Jae-Hong-Kim <newfly101@naver.com>
2024-05-07 22:22:33 +0900

    [Day9] 1-15 당근마켓 clone 만들어보기 - 글쓰기 페이지 구현하기 - FRONT-END에서 html 강제로 create 하기

--- mainAssignment/static/index.js
+++ mainAssignment/static/index.js
@@ -1,6 +1,53 @@
-const renderData = (data) => {
+/* 만들어야 하는 클래스 형태 */
+// <div className="item-list">
+//     <div className="item-list__img">
+//         <img src="asset/main/image.svg" alt="image"/>
+//     </div>
+//     <div className="item-list__info">
+//         <div className="item-list__info-title">게이밍 pc 팝니다</div>
+//         <div className="item-list__info-meta">역삼동 19초 전</div>
+//         <div className="item-list__info-price">100만원</div>
+//     </div>
+// </div>
 
-    data.forEach( (obj) => console.log(obj));
+
+const renderData = (data) => {
+    const main = document.querySelector("main");
+    data.forEach((obj) => {
+        console.log("list => ", obj);
+
+        const itemListDiv = document.createElement("div");
+        itemListDiv.className = 'item-list';
+        main.appendChild(itemListDiv);
+
+        const itemListImageDiv = document.createElement("div");
+        itemListImageDiv.className = 'item-list__img';
+        itemListDiv.appendChild(itemListImageDiv);
+
+        const imageBox = document.createElement("input");
+        imageBox.src = 'asset/main/image.svg';
+        imageBox.alt = 'image';
+        itemListImageDiv.appendChild(imageBox);
+
+        const itemListInfoDiv = document.createElement("div");
+        itemListInfoDiv.className = 'item-list__info';
+        itemListDiv.appendChild(itemListInfoDiv);
+
+        const itemListInfoTitle = document.createElement("div");
+        itemListInfoTitle.className = 'item-list__info-title';
+        const itemListInfoMeta = document.createElement("div");
+        itemListInfoMeta.className = 'item-list__info-meta';
+        const itemListInfoPrice = document.createElement("div");
+        itemListInfoPrice.className = 'item-list__info-price';
+
+        itemListInfoDiv.appendChild(itemListInfoTitle);
+        itemListInfoDiv.appendChild(itemListInfoMeta);
+        itemListInfoDiv.appendChild(itemListInfoPrice);
+
+        // const div = document.createElement("div");
+        // div.innerText = obj.title;
+        // main.appendChild(div);
+    });
 }
 
 
