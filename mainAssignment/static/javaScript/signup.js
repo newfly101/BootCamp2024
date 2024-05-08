491bb2c10899e73ff2c854c4e8157ccd963212eb
Jae-Hong-Kim <newfly101@naver.com>
2024-05-08 22:02:15 +0900

    [Day9] 1-17 당근마켓 clone 만들어보기 - PASSWORD 암호화 부분 구현

--- mainAssignment/static/javaScript/signup.js
+++ mainAssignment/static/javaScript/signup.js
@@ -1,13 +1,21 @@
 const form = document.querySelector('#sign-up-form');
 
 const handleSubmitForm = async (event) => {
-    event.preventDefault();
-    const body = new FormData();
+    event.preventDefault(); // 페이지 전환 기본 동작 중지
+    const formData = new FormData(form);
+    const sha256Password = sha256(formData.get("password"));
+    console.log("암호화전 : ",formData.get("password"));
+
+    formData.set("password", sha256Password);
+    console.log("암호화후 : ",formData.get("password"));
+
     const res = await fetch("/signup", {
         method: 'POST',
-        body: body
+        body: formData
     });
-    console.log(res.json());
+
+    const jsonRes = res.json();
+    console.log(jsonRes);
 }
 
 form.addEventListener("submit", handleSubmitForm);
