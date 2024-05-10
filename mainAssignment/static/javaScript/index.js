c633a12c57beb288a59964920a610a445cc3e8df
Jae-Hong-Kim <newfly101@naver.com>
2024-05-11 03:45:35 +0900

    [Day10] 1-19 당근마켓 clone 만들어보기 - local Storage에 access_token 저장하기

--- mainAssignment/static/javaScript/index.js
+++ mainAssignment/static/javaScript/index.js
@@ -75,8 +75,12 @@ const renderData = (data) => {
 
 const fetchList = async () => {
     const res = await fetch('/items');
+    if (res.status === 401) {
+        alert("로그인이 필요합니다.")
+        window.location.pathname = "/html/login.html";
+        return;
+    }
     const data = await res.json();
-
     // console.log(data);
     renderData(data);
 };
--- mainAssignment/static/javaScript/login.js
+++ mainAssignment/static/javaScript/login.js
@@ -23,7 +23,7 @@ async function getItemsAuth(access_token) {
 const handleSubmitLogin = async (event) => {
     event.preventDefault();
     const formData = new FormData(form);
-    let access_token = null;
+    // let access_token = null;
     // login 한 password 암호화해서 DB로 전달
     const sha256Password = sha256(formData.get("password"));
     formData.set("password", sha256Password);
@@ -45,7 +45,9 @@ const handleSubmitLogin = async (event) => {
         } else if (res.status === '401') {
             console.log("비밀번호가 일치하지 않습니다.");
         } else {
-            access_token = res.access_token;
+            const access_token = res.access_token;
+            window.localStorage.setItem("token",access_token);
+            window.sessionStorage.setItem("token",access_token);
             // console.log("##########:", access_token);
             info.innerText = '로그인 되었습니다.';
             getItemsAuth(access_token); /// <<<<<<<<<<<<<<<<<여기서 오류남
