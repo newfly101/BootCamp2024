6b1075e3d7c1a210f828d57e9351742c8fc48e48
Jae-Hong-Kim <newfly101@naver.com>
2024-05-11 04:24:00 +0900

    [Day10] 1-19 당근마켓 clone 만들어보기 - item list 불러올때 jwt 참고하게 수정

--- mainAssignment/static/javaScript/index.js
+++ mainAssignment/static/javaScript/index.js
@@ -74,7 +74,12 @@ const renderData = (data) => {
 
 
 const fetchList = async () => {
-    const res = await fetch('/items');
+    const accessToken = window.localStorage.getItem('token');
+    const res = await fetch('/items', {
+        headers:{
+            Authorization: `Bearer ${accessToken}`
+        },
+    });
     if (res.status === 401) {
         alert("로그인이 필요합니다.")
         window.location.pathname = "/html/login.html";
