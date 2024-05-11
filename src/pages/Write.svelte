7dd3d816ce2cea3ca4bc8f4002b72e0dad282126
Jae-Hong-Kim <newfly101@naver.com>
2024-05-12 02:24:28 +0900

    [Day11] fireBase - 당근마켓 클론 코딩 - 싱글 어플리케이션 - data값 script와 연동

--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,27 +1,47 @@
+<script>
+    import { getDatabase, ref, set } from "firebase/database";
+
+    let title
+    let price
+    let description
+    let place
+
+    function writeUserDatabase(userId, name, email) {
+        event.preventDefault();
+        const db = getDatabase();
+        set(ref(db, 'users/' + userId), {
+            username: name,
+            email: email,
+        });
+        console.log(`userId:${userId}, name:${name}, email:${email}`);
+    }
+</script>
+
 <main>
+    <button on:click={() => console.log(title, price, description, place)}>버튼</button>
     <form id="write-form">
-        <div>
-            <label for="image">이미지</label>
-            <input type="file" id="image" name="image"/>
-        </div>
+<!--        <div>-->
+<!--            <label for="image">이미지</label>-->
+<!--            <input type="file" id="image" name="image"/>-->
+<!--        </div>-->
         <div>
             <label for="title">제목</label>
-            <input type="text" id="title" name="title" required/>
+            <input type="text" id="title" name="title" bind:value={title} required/>
         </div>
         <div>
             <label for="price">가격</label>
-            <input type="number" id="price" name="price" required/>
+            <input type="number" id="price" name="price" bind:value={price} required/>
         </div>
         <div>
             <label for="description">설명</label>
-            <input type="text" id="description" name="description"/>
+            <input type="text" id="description" bind:value={description} name="description"/>
         </div>
         <div>
             <label for="place">장소</label>
-            <input type="text" id="place"  name="place"/>
+            <input type="text" id="place"  name="place" bind:value={place}/>
         </div>
         <div>
-            <button type="submit" id="write-submit-button">제출</button>
+            <button type="submit" id="write-submit-button" on:click={(event) => writeUserDatabase('aaa','aaa','aaa@naver.com')}>제출</button>
         </div>
     </form>
 </main>
