cfd70458d20c934ccb0a26aba574f77021b619f3
Jae-Hong-Kim <newfly101@naver.com>
2024-05-12 06:36:19 +0900

    [Day12] fireBase - .gitignore 추가

--- firebase.js
+++ firebase.js
@@ -5,11 +5,9 @@ import { getStorage } from "firebase/storage";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    // ...
-    // The value of `databaseURL` depends on the location of the database
-    databaseURL: "https://bootcamp2024-ef5a8-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    storageBucket: "gs://bootcamp2024-ef5a8.appspot.com",
+
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
