109c8b7161239c58ef86064634c4edb2713ab5c3
Jae-Hong-Kim <newfly101@naver.com>
2024-05-12 05:32:25 +0900

    [Day11] fireBase - 당근마켓 클론 코딩 - 싱글 어플리케이션 - onMount() 추가 : 실시간 rendering

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -2,7 +2,8 @@
     import Footer from "../components/Footer.svelte";
     import Header from "../components/Header.svelte";
 
-    import { getDatabase, ref, onValue } from "firebase/database";
+    import { getDatabase, ref, onValue} from "firebase/database";
+    import {onMount} from "svelte";
 
     // 반응형으로 rendering 해주는 것을 선언해줄때
     // $: 로 사용
@@ -12,13 +13,18 @@
 
 
     // 화면이 onMount 되었을 때 가끔 render이 안되는 경우가 발생하기 때문에 선언해주는 것
-
-
-    onValue(itemsList, (snapshot) => {
-        const data = snapshot.val();
-        items = Object.values(data);
-        console.log(items);
+    // <script> 는 화면이 시작 되었을 때 1번만 실행한다.
+    // 화면이 보여질 때 마다 실행하고 싶다면 onMount() 를 사용해야 한다.
+    // firebase와 client가 socket으로 연결되어 있기 때문에 실시간 업데이트가 가능하다.
+    onMount(() => {
+        onValue(itemsList, (snapshot) => {
+            const data = snapshot.val();
+            items = Object.values(data);
+            console.log(items);
+        });
     });
+
+
 </script>
 
 <Header />
