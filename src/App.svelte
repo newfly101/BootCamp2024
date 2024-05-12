8a3c4163ceadb3152e0296b8b520e33d0207585e
Jae-Hong-Kim <newfly101@naver.com>
2024-05-12 17:51:37 +0900

    [Day12] fireBase - social login - localStorage의 token값 이용해 출력하는 공식문서페이지 입력(버그난다.이대로x)

--- src/App.svelte
+++ src/App.svelte
@@ -17,6 +17,28 @@
   //
   // let userLogin = false;
 
+  function handleCredentialResponse(response) {
+    // Build Firebase credential with the Google ID token.
+    const idToken = response.credential;
+    const credential = GoogleAuthProvider.credential(idToken);
+
+    // Sign in with credential from the Google user.
+    signInWithCredential(auth, credential).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The credential that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+  }
+
+
+
+
+
   const routes = {
     '/': Main,
     '/signup': SignUp,
