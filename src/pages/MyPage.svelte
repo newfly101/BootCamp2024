d96dc89cc044b1e83f64ff683a850c8e4f6ad793
Jae-Hong-Kim <newfly101@naver.com>
2024-05-12 20:01:16 +0900

    [Day12] fireBase - 로그아웃 구현하기 완료

--- src/pages/MyPage.svelte
+++ src/pages/MyPage.svelte
@@ -1,8 +1,11 @@
 <script>
     import Header from "../components/Header.svelte";
     import Footer from "../components/Footer.svelte";
+    import {userStore} from "../Store.js";
 
     const logout = () => {
+        window.localStorage.removeItem('token');
+        userStore.set(null);
 
     };
 </script>
