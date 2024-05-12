b3241d46a1437deb8697f10397bf39e49eac0b94
Jae-Hong-Kim <newfly101@naver.com>
2024-05-12 17:24:37 +0900

    [Day12] fireBase - social login - google 로그인 추가

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,6 +1,34 @@
 <script>
     import Header from "../components/Header.svelte";
     import Footer from "../components/Footer.svelte";
+    import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    const loginWithGoogle = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+    }
+
+
 </script>
 
 <Header />
@@ -19,6 +47,7 @@
             <button type="submit">로그인</button>
         </div>
     </form>
+    <button on:click={loginWithGoogle}>구글 로그인</button>
 
     <div id="info"></div>
 </main>
